5d187496e8978df6b48cd0d64fab32c49116298d
Wanderley Drumond <wanderley.drumond@gmail.com>
2022-12-16 18:28:08 +0000

    - Changed enhanced for to lambda expressions

--- src/main/java/pt/drumond/rumosdigitalbank/repository/implementations/AccountListRepositoryImplementation.java
+++ src/main/java/pt/drumond/rumosdigitalbank/repository/implementations/AccountListRepositoryImplementation.java
@@ -97,14 +97,16 @@ public class AccountListRepositoryImplementation implements AccountRepository {
 
     @Override
     public Account findByCardSerialNumber(String cardSerialNumber) {
-        for (Account accountElement : tableAccounts) { // procuro dentro da minha tabela contas
+        /*for (Account accountElement : tableAccounts) { // procuro dentro da minha tabela contas
             for (Card cardElement : accountElement.getCards()) { // procuro dentro da minha lista de cartões
                 if (cardElement.getSerialNumber().equals(cardSerialNumber)) { // Se algum número de série de algum desses cartões for igual ao que eu passei por argumento
                     return accountElement; // retorno a conta
                 }
             }
         }
-        return null; // Caso não ache nenhum cartão com o número de série fornecido
+        return null; // Caso não ache nenhum cartão com o número de série fornecido*/
+
+        return tableAccounts.stream().filter(accountElement -> accountElement.getCards().stream().anyMatch(cardElement -> cardElement.getSerialNumber().equals(cardSerialNumber))).findFirst().orElse(null);
     }
 
     /**
